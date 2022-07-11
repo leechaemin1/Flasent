7c804edb641c6fe86a3b2f0eaf6c2fdf79d633c6
Danbee Kim <kdb2360@gmail.com>
2022-07-11 15:37:51 +0900

    init2

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+from pymongo import MongoClient
+import certifi
+
+client = MongoClient('mongodb+srv://diana:sparta@cluster0.oscy4t6.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=certifi.where())
+db = client.dbsparta
+
+
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/login')
+def home():
+    return render_template('login.html')
+
+@app.route('/membership')
+def home():
+    return render_template('membership.html')
+
+@app.route('/post')
+def home():
+    return render_template('post.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
