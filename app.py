4ece6f94f1db86a38ccb54abdf7425ddc43cfdf0
soyeon102 <csea9000@naver.com>
2022-07-14 00:26:21 +0900

    회원가입 및 로그인 구현

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-import requests
 from werkzeug.utils import secure_filename
 from datetime import datetime, timedelta
-import jwt
-import hashlib
-import datetime
+
+
 
 app = Flask(__name__)
 
@@ -22,13 +23,13 @@ SECRET_KEY = 'SPARTA'
 @app.route('/')
 def home():
     token_receive = request.cookies.get('mytoken')
-    # try:
-    #     payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-    #     return render_template('main.html')
-    # except jwt.ExpiredSignatureError:
-    #     return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    # except jwt.exceptions.DecodeError:
-    #     return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('main.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template('main.html')
 
 @app.route('/login')
@@ -52,7 +53,7 @@ def sign_in():
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').encode().decode()
 
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
@@ -68,6 +69,16 @@ def post(id):
     flower_list = list(db.flowers.find({}, {'_id': False}))
     if (int(id) >= len(flower_list)):
         return redirect('/')
+
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('main.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
     return render_template('post.html', id=id, doc=flower_list)
 
 @app.route("/flower", methods=["GET"])
@@ -103,6 +114,28 @@ def review_post():
     return jsonify({'msg': '저장 완료!'})
 
 
+@app.route('/membership/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/membership/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success', 'msg': '회원가입이 완료되었습니다.'})
 
 
 
