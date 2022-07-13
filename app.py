0384c0b3e08f6458f799c29c6a2f5c4bde88909e
soyeon102 <csea9000@naver.com>
2022-07-13 10:27:43 +0900

    로그인페이지 작성

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-import json
-
-from flask import Flask, render_template, request, jsonify, redirect
+from pymongo import MongoClient
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import requests
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+import jwt
+import hashlib
+import datetime
 
 app = Flask(__name__)
 
@@ -12,15 +16,46 @@ client = MongoClient('mongodb+srv://diana:sparta@cluster0.oscy4t6.mongodb.net/Cl
                      tlsCAFile=certifi.where())
 db = client.flower
 
+SECRET_KEY = 'SPARTA'
 
-@app.route('/')
+
+@app.route('/', methods=["GET"])
 def home():
+    category_receive = request.args.get('category_give')
+    print(category_receive)
     flower_list = list(db.flowers.find({}, {'_id': False}))
+    # for f_list in flower_list:
+    #     if category_receive is None:
+    #         print('afdsfsdf')
+    #     if category_receive in f_list['category']:
+    #         print('cate', category_receive, 'f_list', f_list)
     return render_template('main.html', lists=flower_list)
 
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 @app.route('/membership')
 def membership():
@@ -32,14 +67,6 @@ def post(id):
     if (int(id) >= len(flower_list)):
         return redirect('/')
     return render_template('post.html', id=id, doc=flower_list)
-
-@app.route("/category", methods=["GET"])
-def category():
-    category_receive = request.args.get('category_give')
-    print(category_receive)
-    return render_template('main.html', category_receive=category_receive)
-
-
 
 
 
