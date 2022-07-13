722a473c3a9a633063de4861bec063b7753182fb
soyeon102 <csea9000@naver.com>
2022-07-13 11:03:32 +0900

    메인페이지 진입시 로그인 페이지 이동

--- app.py
+++ app.py
@@ -21,11 +21,21 @@ SECRET_KEY = 'SPARTA'
 
 @app.route('/')
 def home():
+    token_receive = request.cookies.get('mytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return render_template('main.html')
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
     return render_template('main.html')
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+    # return render_template('login.html')
 
 
 @app.route('/sign_in', methods=['POST'])
